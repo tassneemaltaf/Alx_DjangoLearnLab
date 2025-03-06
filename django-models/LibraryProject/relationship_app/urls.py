5fd59b762d931c966550c7b9befaad0fdbec11fe
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-06 11:10:27 +0100

    Commas added on urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import book_list, LibraryDetailView
 
 urlpatterns = [
-    path("", view=book_list, name="book_list")
+    path("", view=book_list, name="book_list"),
     path("", view=LibraryDetailView, name="LibraryDetailView")
 ]
