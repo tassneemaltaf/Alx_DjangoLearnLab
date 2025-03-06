1367f327a70c3a1daba505420909770db345f0da
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-06 11:08:05 +0100

    Task 1 changes added

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import book_list, LibraryDetailView
+
+urlpatterns = [
+    path("", view=book_list, name="book_list")
+    path("", view=LibraryDetailView, name="LibraryDetailView")
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book, Library
 
-# Create your views here.
+def book_list(request):
+  books = Book.objects.all()
+  context = {'list of book titles and their authors': books}
+  return render(request, 'relationship_app/list_books.html', context)
+
+
+class LibraryDetailView(DetailView):
+  model = Library
+  template_name = 'relationship_app/library_detail.html'
+
+def get_context_data(self, **kwargs) -> dict[str, Any]:
+  context = super().get_context_data(**kwargs)
+  library = self.get_object()
+  context["books"] = Book.objects.filter(library=library)
+  return context
