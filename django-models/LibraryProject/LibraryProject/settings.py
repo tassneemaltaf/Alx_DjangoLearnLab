9c155487f73b92e72a892cc5460ea12dd792b55a
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-10 18:16:53 +0100

    Add changes

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "relationship_app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
