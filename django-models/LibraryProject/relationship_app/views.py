f7cfe34073da3f19cee50ef2561eb26a0293799c
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-10 16:43:16 +0100

    Mod added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
