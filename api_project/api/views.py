a4d96418a24d5fe2a754a7714a6ec91682fa34a5
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-16 06:50:31 +0100

    CRUD Ops

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,11 @@
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 class BookList(generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
