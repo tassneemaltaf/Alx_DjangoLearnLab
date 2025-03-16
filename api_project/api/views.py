c86675d5492747e1d7a431f09a8836b6b262f251
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-16 06:14:02 +0100

    Changes on views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
