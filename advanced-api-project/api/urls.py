d05b0a246fcf94a0ef49b19125d18adc878f22fb
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-20 11:52:13 +0100

    Testing second time

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import ListView, CreateView, DetailView, UpdateView, DeleteView
 urlpatterns = [
     path("books/", view=ListView.as_view(), name="list_view"),
     path("books/<int:pk>/", view=DetailView.as_view(), name="detail_view"),
-    path("books/new/", view=CreateView.as_view(), name="create_view"),
-    path("books/edit/<int:pk>/", view=UpdateView.as_view(), name="update_view"),
+    path("books/create/", view=CreateView.as_view(), name="create_view"),
+    path("books/update/", view=UpdateView.as_view(), name="update_view"),
     path("books/delete/<int:pk>/", view=DeleteView.as_view(), name="delete_view"),
 ]
