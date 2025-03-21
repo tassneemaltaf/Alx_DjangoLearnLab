4422377613c307dbb8e0f6d9f998bfe6d0a003df
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-21 09:30:00 +0100

    Task 2 modification

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated", "IsAuthenticated
 
 class ListView(generics.ListAPIView):
   queryset = Book.objects.all()
