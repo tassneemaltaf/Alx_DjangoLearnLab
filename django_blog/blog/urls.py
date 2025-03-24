7658a8efd67442d579ad50f8e8c1f28835ac221c
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-24 13:07:57 +0100

    Task 1 modified

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import SignUpView
+from .views import RegisterView
 
 urlpatterns = [
   path('accounts/', include('django.contrib.auth.urls')),
-  path('accounts/profile', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
-  path('signup/', SignUpView.as_view(), name='signup'),
+  path('accounts/profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
+  path('register/', RegisterView.as_view(), name='register'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 
-class SignUpView(CreateView):
+class RegisterView(CreateView):
   form_class = UserCreationForm #Uses Django’s built-in form for user registration.
-  template_name = 'registration/signup.html'  #Path to signup template
+  template_name = 'registration/register.html'  #Path to signup template
   success_url = reverse_lazy('login')  #Redirects to login after signup
