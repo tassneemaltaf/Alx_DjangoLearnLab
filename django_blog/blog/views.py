7e9eaef469db888204650cdf0fba23a01dce34a1
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-24 13:02:39 +0100

    Task 2 added

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+class SignUpView(CreateView):
+  form_class = UserCreationForm #Uses Django’s built-in form for user registration.
+  template_name = 'registration/signup.html'  #Path to signup template
+  success_url = reverse_lazy('login')  #Redirects to login after signup
