03092df9f4394fec4c89a60dca6a6feba9484281
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-25 09:14:41 +0100

    Added Login and Logout urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from .views import RegisterView
+from django.contrib.auth.views import LoginView , LogoutView
 
 urlpatterns = [
-  path('accounts/', include('django.contrib.auth.urls')),
+  path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
   path('accounts/profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
   path('register/', RegisterView.as_view(), name='register'),
   path('', TemplateView.as_view(template_name='blog/base.html'), name='home'),
