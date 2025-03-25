b3f966335f594a26c4ba3d34ca7fd9c99d17a291
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-03-25 10:57:12 +0100

    User changed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import RegisterView
-from django.contrib.auth.views import LoginView , LogoutView
+from .views import RegisterView, submit
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-  path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+  path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
   path('accounts/profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
   path('register/', RegisterView.as_view(), name='register'),
   path('', TemplateView.as_view(template_name='blog/base.html'), name='home'),
-  path('', TemplateView.as_view(template_name='blog/base.html'), name='posts'),
+  path('posts/', submit, name='posts'),
+  path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+  path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,25 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+
+User = get_user_model
 
 class RegisterView(CreateView):
   form_class = UserCreationForm #Uses Django’s built-in form for user registration.
   template_name = 'registration/register.html'  #Path to signup template
   success_url = reverse_lazy('login')  #Redirects to login after signup
+
+@login_required
+def profile_view(request):
+  return render(request, 'accounts/profile.html')
+
+def submit(request):
+  if request.method == "POST":
+    user = request.user
+    user.username = request.POST["username"]
+    user.save()
+    return redirect("profile")
+  return render(request, "blog/profile.html", {"msg": "username updated"})
