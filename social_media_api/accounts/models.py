655a801bed40fafd5db954f0c54da8a3d337619e
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-04-05 16:53:14 +0200

    Models created

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+    followers = models.IntegerField(default=0)
+
