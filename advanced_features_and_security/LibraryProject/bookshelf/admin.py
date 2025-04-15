7e33e267c8a38fdc13320c692522873b6178d7dc
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-04-15 18:35:46 +0200

    Admin configured for custom user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
   list_filter = ('title', 'author', 'publication_year')
   search_fields = ('title', 'author')
 
+class CustomUserAdmin(admin.ModelAdmin):
+  list_filter = ('email', 'password', 'date_of_birth', 'profile_photo')
+  search_fields = ('email', 'date_of_birth')
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
